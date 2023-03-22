236663e45a9ada112f09f26da9b3a3e90fed85d7
osama <syedosama478@gmail.com>
2023-03-23 02:38:35 +0500

    Add Chapter 5

--- chapter_05.js
+++ chapter_05.js
@@ -103,7 +103,7 @@ console.log(myAccount)
 //BUILD AN EXPENSE TRACKER
 
 
-let account = {
+let accountMy = {
 	name: 'Syed Muhammad Osama',
 	income: 0,
 	expenses:0
@@ -111,15 +111,15 @@ let account = {
 
 let addExpenses = function (account , amount){
 	account.expenses = account.expenses + amount
-	account.income = acount.income - amount
 }
 let addIncome = function (account , amount){
-	account.income = accoun.income + amount
+	account.income = account.income + amount
 }
 let getAccountSummary = function (account){
-	return{
-		summary:`Account for ${name} has $${income}. $${expenses} in expenses.`
-}}
+	let balance = account.income - account.expenses
+
+	return	`Account for ${accountMy.name} has $${balance} balance.$${accountMy.income} income.  $${accountMy.expenses} in expenses.`
+}
 
 let resetAccount = function (account){
 	account.expenses = 0
@@ -127,10 +127,173 @@ let resetAccount = function (account){
 }
 
  
-let accountDetail = function (account){
-	addIncome(account)
-	addExpense(account)
-	getAccountSummary(account)
+addIncome(accountMy , 5000)
+addExpenses(accountMy , 100)
+addExpenses(accountMy , 500)
+addExpenses(accountMy , 1200)
+console.log(getAccountSummary(accountMy)) 
+//we can do above console.log instead of below lines
+//let detail = getAccountSummary(accountMy)
+//console.log(detail)
+resetAccount(accountMy)
+console.log(getAccountSummary(accountMy)) 
+
+
+
+//METHODS
+
+
+let restaurant = {
+	name: 'La Montana',
+	guestCapacity: 75,
+	guestCount: 0,
+	checkAvailability: function (partySize){
+		let seatsLeft = this.guestCapacity - this.guestCount
+		return partySize <= seatsLeft}, 
+	seatParty: function (no_of_guests_entered){
+		this.guestCount = this.guestCount + no_of_guests_entered
+		return this.guestCount},
+	removeParty: function (no_of_guests_left){
+		this.guestCount = this.guestCount - no_of_guests_left
+		return this.guestCount
+
+}}
+
+
+restaurant.seatParty(72)
+console.log(restaurant.checkAvailability(4))
+restaurant.removeParty(5)
+console.log(restaurant.checkAvailability(4))
+
+
+
+//EXPLORING STRING METHODS
+
+
+
+let myName = 'Syed Muhammad Osama'
+
+//length property
+console.log(myName.length)
+
+//COnvert to uppercase
+console.log(myName.toUpperCase(myName))
+
+// convert to lower case
+console.log(myName.toLowerCase())
+
+//Include method
+let password = 'abc123password'
+console.log(password.includes('password'))
+
+//TRIM
+console.log(myName.trim())
+
+
+
+//CHALLENGE AREA
+
+let isValidPassword = function (password){
+
+	return password.length >8 && !password.includes('password')
+	//if (password.length >8 && !password.includes('password')){
+	//return true
+//}	else{
+	//return false
+//we can use one line return instead of this all if statements
 }
 
+console.log(isValidPassword('abcjkadhfkkjlbaskdfpassword'))
+
+
+
+
+
+
+
+//EXPLORING NUMBERS METHOD
+
+
+
+let num = 103.941
+
+//To fixed method. it will show 2 numbers after decimal point as we have passed 2 in below line
+console.log(num.toFixed(2))
+
+//ROund
+console.log(Math.round(num))
+
+//Floor
+console.log(Math.floor(num))  //it will round down to nearest integer 
+
+//Ceiling
+console.log(Math.ceil(num)) // it will round up to nearest integer
+
+//Random
+console.log(Math.random(num)) // it will create random number between 0 and 1 not 1 0.999999999999999999
+
+//random more detail
+
+let min = 10
+let max = 20
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min //it will give whole number range between 10 and 20
+console.log(randomNum)
+
+
+
+
+//CHALLENGE AREA
+
+
+
+let min1 = 1
+let max1 = 5
+let randomNum1 = Math.floor(Math.random() * (max1 - min1 + 1)) + min1 //it will give whole number range between 10 and 20
+let makeGuess = function(number){
+	return number === randomNum1
+}
+console.log(randomNum1)
+console.log(makeGuess(1))
+
+
+
+
+
+//CONSTANTS VARIABLES
+
+
+
+
+const isRaining = true // it is replacement as let but difference is const cannot be reassigned like we have assigned israining after false it will give error but for let it will modify israining to false
+//isRaining = false
+
+console.log(isRaining)
+
+const person2 = {
+	age: 23
+}
+//person{}// it will give error but we can modify object properties same as let object
+
+person2.age = 18 // it is corect we can do this
+
+//we uses const because itt makes read the code easy. if someone else is reading code variable with const will make that person easy that this will not be changed so he does not have to track it
+
+
+
+
+
+// BONUS: VARIABLES WITH VAR
+
+
+var firstName = 'Osama'
+firstName = 'Syed'
+
+var firstName = 'nouman'  // it will not show error that firstName is already declared it will assign new value to it so this is dangerous so this is not used in javascript now
+console.log(firstName)
+
+if(10 === 10){
+	var lastName = 'hassan' // it will not give error it is function scoped not block scoped if we use let instead of var in this line wq will get an error
+}
+
+console.log(lastName)
 
